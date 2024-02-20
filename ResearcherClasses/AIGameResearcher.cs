04ba470c812b149bffd4bc0c219f841f6e734bea
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-02-20 13:51:59 +0000

    Start developing exception handling for common exceptions with AIGameResearcher class

--- ResearcherClasses/AIGameResearcher.cs
+++ ResearcherClasses/AIGameResearcher.cs
@@ -1,3 +1,5 @@
+using CSharpBackend.API.Models.DataTransferObjects;
+
 namespace CSharpBackend.ResearcherClasses
 {
 
@@ -7,46 +9,65 @@ namespace CSharpBackend.ResearcherClasses
     
     
 
-    public class AIGameResearcher(string BoardGameName)
-    {
-        string boardGameName = BoardGameName;
-    }
+        public AIGameResearcher(string BoardGameName)
+        {
+
+            if (string.IsNullOrWhiteSpace(BoardGameName))
+            {
+                throw new ArgumentNullException(nameof(BoardGameName),"BoardGameName must not be null...");
+            }
+
+            string boardGameName = BoardGameName;
+        
+        }
 
 
     public async Task<BoardGameDto?> GetBoardGameResearchObj()
     {
+            var boardGameResearchObject = await ResearchGame();
+            return boardGameResearchObject;
+    }
+
+    private async Task<BoardGameDto?> ResearchGame()
+    {
         try 
         {
-            var boardGameResearchObject = TryToGetBoardGameResearchObj();
+            var boardGameResearchObject = await TryToResearchGame();
             return boardGameResearchObject;
         }
         catch (Exception exception)
         {
-            Console.WriteLine(exception);
+            Console.WriteLine(exception);   
         }
-    }
-
-
-    public async Task<BoardGameDto?> TryToGetBoardGameResearchObj()
-    {
-        var boardGameResearchObject = await ResearchGame();
-        
-        var isRealBoardGame = boardGameResearchObject["isRealBoardGame"];
-        var isNewToDatabase = boardGameResearchObject["isNewToDatabase"];
-
-        if (isRealBoardGame && isNewToDatabase)
+        catch ()
         {
-            return boardGameResearchObject;
+
         }
-        return {};
+        catch ()
+        {
+
+
+        }
     }
 
 
 
-    public async Task<BoardGameDto?> TryToResearchGame()
+    private async Task<BoardGameDto?> TryToResearchGame()
     {
 
-        var openAIKey = new(Environment.GetEnvironmentVariable("OPEN_AI_KEY"));
+        var isNewToDatabase = await checkIsNewGameToDB(boardGameName);
+        if (!isNewToDatabase)
+        {
+            return new BoardGameDto();
+        }
+
+        var openAIKey = Environment.GetEnvironmentVariable("OPEN_AI_KEY");
+       
+        if (string.IsNullOrEmpty(openAIKey))
+        {
+            throw new Exception("Open AI API key is empty or undefined...");
+        }
+
         var client = new OpenAIClient(openAIKey, new OpenAIClientOptions());
 
         var chatCompletionOptions = new ChatCompletionsOptions()
@@ -58,8 +79,35 @@ namespace CSharpBackend.ResearcherClasses
 
         };
 
+        try 
+        {
+            //Connect to Open AI API and make a request
 
 
+        }
+        catch ()//Exception produced as a result of Open AI API Connection
+        {
+            throw new Exception("Error connecting to Open AI API");
+        }
+        catch ()//Exception produced as a result of lack of Open AI API credits
+        {
+            throw new Exception("Error occurred to lack of Open AI API credits");
+        }
+        catch ()//Exception produced as a result of lack of Open AI API credits
+        {
+            throw new Exception("Error occurred to lack of Open AI API credits");
+        }
+
+
+        var isRealBoardGame = gameResearchObject["boardGameExists"];
+        if (!isRealBoardGame)
+        {
+            return new BoardGameDto();
+        }
+
+        var boardGameResearchObject = createBoardGameDto(gameResearchObject);
+        
+        
     }
 
     
